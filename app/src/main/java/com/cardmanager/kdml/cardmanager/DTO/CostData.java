fd4c7cb81098ac4bf2372823104d5b8ee3a55b59
home desctop <bhkdm8571@gmail.com>
2016-08-24 01:23:39 +0900

    0824

--- app/src/main/java/com/cardmanager/kdml/cardmanager/DTO/CostData.java
+++ app/src/main/java/com/cardmanager/kdml/cardmanager/DTO/CostData.java
@@ -1,5 +1,10 @@
 package com.cardmanager.kdml.cardmanager.DTO;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by 김대명 on 2016-08-08.
  */
@@ -10,6 +15,8 @@ public class CostData {
     private String dateTimeOrigin;
     private String dateTime;
     private String email;
+    private List<CostData> test;
+    private Map<String,CostData> maptest;
     public CostData(){}
 
     public CostData(String dateTime, String cardName, String yearMonth, String cost, String dateTimeOrigin,String email) {
@@ -19,6 +26,18 @@ public class CostData {
         this.cost = cost;
         this.dateTimeOrigin = dateTimeOrigin;
         this.email = email;
+
+        test = new ArrayList<>();
+
+        test.add( new CostData("test1","test1","test1"));
+        test.add( new CostData("test2","test2","test2"));
+
+        maptest = new HashMap<>();
+
+        maptest.put("201608",new CostData("test1","test1","test1"));
+        maptest.put("201607",new CostData("test2","test2","test2"));
+
+
     }
 
     public CostData(String cardName, String yearMonth, String cost) {
@@ -73,5 +92,21 @@ public class CostData {
 
     public void setDateTime(String dateTime) {
         this.dateTime = dateTime;
+    }
+
+    public List<CostData> getTest() {
+        return test;
+    }
+
+    public void setTest(List<CostData> test) {
+        this.test = test;
+    }
+
+    public Map<String, CostData> getMaptest() {
+        return maptest;
+    }
+
+    public void setMaptest(Map<String, CostData> maptest) {
+        this.maptest = maptest;
     }
 }
